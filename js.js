9710def6f6c52596f7cd3a853eb9262b3999cf6f
Guy Halfon <104530982+xHALFON@users.noreply.github.com>
2022-05-15 17:16:40 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,25 @@
+'use strict'
+
+var divel = document.getElementById('dropdownul');
+var divel2 = document.getElementById('dropdownul2');
+
+function fun(){
+    var a = divel.classList.toggle('hidden');
+    if(a){
+        document.getElementById("a").src = "images/icon-arrow-down.svg";
+    }else{
+        document.getElementById("a").src = "images/icon-arrow-up.svg";
+    }
+};
+function fun2(){
+   var s = divel2.classList.toggle('hidden');
+    if(s){
+        document.getElementById("s").src = "images/icon-arrow-down.svg";
+    }else{
+        document.getElementById("s").src = "images/icon-arrow-up.svg";
+    }
+};
+
+
+document.getElementById('qq').addEventListener('click', fun);
+document.getElementById('qq2').addEventListener('click', fun2);
