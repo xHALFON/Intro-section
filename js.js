bcee2cf868ef0ab5fc81704681a1ff786b525168
Guy Halfon <104530982+xHALFON@users.noreply.github.com>
2022-05-17 15:13:41 +0300

    Update js.js

--- js.js
+++ js.js
@@ -1,1 +1,27 @@
+'use strict'
 
+var divel = document.getElementById('dropdownul');
+var divel2 = document.getElementById('dropdownul2');
+
+function fun(){
+    var a = divel.classList.toggle('hidden');
+    divel2.classList.add('hidden');
+    if(a){
+        document.getElementById("a").src = "images/icon-arrow-down.svg";
+    }else{
+        document.getElementById("a").src = "images/icon-arrow-up.svg";
+    }
+};
+function fun2(){
+   var s = divel2.classList.toggle('hidden');
+    divel1.classList.add('hidden');
+    if(s){
+        document.getElementById("s").src = "images/icon-arrow-down.svg";
+    }else{
+        document.getElementById("s").src = "images/icon-arrow-up.svg";
+    }
+};
+
+
+document.getElementById('qq').addEventListener('click', fun);
+document.getElementById('qq2').addEventListener('click', fun2);
